729ffc1791578109277210b6c73a1a58e12d5827
jnsamines <jn.samines@gmail.com>
2014-04-23 09:03:34 -0600

    Finalizado problema final. Servidor json y fechas.

--- date-json/date.js
+++ date-json/date.js
@@ -3,16 +3,27 @@ var http = require('http'),
     port = process.argv[2];
 
 var server = http.createServer(function(request, response){
-    var uri = url.parse(request.url);
+    var uri = url.parse(request.url, true);
 
-    if(uri.pathname === '/api/unixtime'){
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    var format = uri.query.iso,
+		date = new Date(format);
 	
+    if(uri.pathname === '/api/unixtime'){
+		var res = {
+			unixtime : Number(date)
+		};
+		response.end(JSON.stringify(res));
     }
 
-    if(uri.pathname === "/api/parsetime"){
-    	
+    if(uri.pathname === '/api/parsetime'){
+		
+		var res = {
+			hour : date.getHours(),
+			minute : date.getMinutes(),
+			second : date.getSeconds()
+		};
+		response.end(JSON.stringify(res));
     }
-
-
-
 }).listen(port);
+console.log('Servidor iniciado en el puerto ' +  port);
